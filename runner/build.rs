3895d3eaf7672e9bdc765ee52bb016d0dffc258e
Mikhail Katychev <mkatych@gmail.com>
2026-04-20 15:57:18 -0500

    clarified lack of pulley

--- runner/build.rs
+++ runner/build.rs
@@ -9,15 +9,14 @@ pub fn compile_components(cargo_artifact_env_key: &'static str, components: &[&'
         let component_path = std::env::var_os(cargo_artifact_env_key).unwrap();
         let wasm_bytes =
             std::fs::read(Path::new(&component_path)).expect("unable to read component path");
-        let cwasm = compile_pulley(&wasm_bytes);
+        let cwasm = compile_component(&wasm_bytes);
 
         let out_dir = std::env::var_os("OUT_DIR").expect("OUT_DIR missing");
         let out_path = Path::new(&out_dir).join(format!("{component}.cwasm"));
-        std::fs::write(&out_path, &cwasm)
-            .expect("unable to write pulley compiled artifact to disk");
+        std::fs::write(&out_path, &cwasm).expect("unable to write compiled artifact to disk");
     }
 }
-pub fn compile_pulley(wasm_bytes: &[u8]) -> Vec<u8> {
+pub fn compile_component(wasm_bytes: &[u8]) -> Vec<u8> {
     // use shared config to guarantee compatibility
     let config = wasmtime::Config::default();
 
@@ -28,7 +27,6 @@ pub fn compile_pulley(wasm_bytes: &[u8]) -> Vec<u8> {
         .encode()
         .expect("failed to encode wasm module bytes");
 
-    // compile wasm component to pulley using wasmtime engine
     let engine = wasmtime::Engine::new(&config).unwrap();
     let binary = engine
         .precompile_component(&component_bytes)
